cc035538546b0022d44a71edfbdb28935651ae31
Scott R. Santos <santos@auburn.edu>
2018-07-12 23:41:01 -0500

    Various tweaks

--- mic_toggle.sh
+++ mic_toggle.sh
@@ -5,10 +5,10 @@ TOGGLE="/tmp/mic_mute_toggled.pid"
 if [ ! -e $TOGGLE ]; then
     touch $TOGGLE
     /usr/bin/amixer -q sset Capture toggle
-    notify-send 'Internal Mic' 'Enabled' -i /usr/share/icons/gnome/48x48/status/microphone-sensitivity-high.png --expire-time=6000
+    notify-send 'Internal Mic' 'Enabled' -i /home/srsantos/.config/dunst/icons/microphone-sensitivity-high.svg --expire-time=6000
 else
     rm -f $TOGGLE
     /usr/bin/amixer -q sset Capture toggle
-    notify-send 'Internal Mic' 'Disabled' -i /usr/share/icons/gnome/48x48/status/microphone-sensitivity-muted.png --expire-time=6000
+    notify-send 'Internal Mic' 'Disabled' -i /home/srsantos/.config/dunst/icons/microphone-sensitivity-muted.svg --expire-time=6000
 fi
 
