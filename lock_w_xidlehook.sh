e6a63d857ed4ebc782b072feb6d8b994a09374f4
Scott R. Santos <santos@auburn.edu>
2023-02-01 10:41:47 -0500

    Added dimmer binady and xidelhook script

---
+++ lock_w_xidlehook.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Only exported variables can be used within the timer's command.
+export PRIMARY_DISPLAY="$(/usr/bin/xrandr | /usr/bin/awk '/ primary/{print $1}')"
+
+# Run xidlehook
+/usr/bin/xidlehook \
+  `# Reset the idle timer after woken from suspend` \
+  --detect-sleep \
+  `# Don't lock when there's a fullscreen application` \
+  --not-when-fullscreen \
+  `# Don't lock when there's audio playing` \
+  --not-when-audio \
+  `# Dim the screen after 20 mins, undim if user becomes active` \
+  --timer 1200 \
+    '/usr/bin/xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+    '/usr/bin/xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+  `# Undim & lock after 2 more mins` \
+  --timer 120 \
+    '/usr/bin/xrandr --output "$PRIMARY_DISPLAY" --brightness 1; /usr/bin/i3lock -f -b -i /home/srsantos/.i3/wallpapers/dark-metal-grids/dark-metal-grid-8_1920x1080.png' \
+    ''
